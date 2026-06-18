b9b442aa4f524e23109720b4c4a32129f4ede329
Elias <eliasquial24@gmail.com>
2026-06-18 12:20:49 -0600

    db(#10): crear tabla propuesta_elemento_afectado para guardar el elemento que esta pendiente

--- database/proyecto-air.sql
+++ database/proyecto-air.sql
@@ -28,6 +28,7 @@ DROP TABLE IF EXISTS nombramiento                 CASCADE;
 DROP TABLE IF EXISTS asistencia_sesion_plenaria   CASCADE;
 DROP TABLE IF EXISTS resolucion                   CASCADE;
 DROP TABLE IF EXISTS punto_agenda                 CASCADE;
+DROP TABLE IF EXISTS propuesta_elemento_afectado CASCADE;
 DROP TABLE IF EXISTS proponente_propuesta         CASCADE;
 DROP TABLE IF EXISTS bitacora_propuesta           CASCADE;
 DROP TABLE IF EXISTS propuesta                    CASCADE;
@@ -233,6 +234,14 @@ CREATE TABLE proponente_propuesta (
     id_asambleista          INT       NOT NULL REFERENCES asambleista(asambleista_id),
     fecha_registro          TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (id_propuesta, id_asambleista)
+);
+CREATE TABLE IF NOT EXISTS propuesta_elemento_afectado (
+          id_propuesta_elemento SERIAL PRIMARY KEY,
+          id_propuesta          INT NOT NULL REFERENCES propuesta(id_propuesta) ON DELETE CASCADE,
+          id_elemento           INT NOT NULL REFERENCES elemento_normativo(id_elemento),
+          modalidad_reforma     VARCHAR(50) NOT NULL,
+          cuerpo_propuesta      TEXT,
+          link_documentacion    TEXT
 );
 
 -- Puente entre las sesiones y propuestas (cuando suceden dentro de la sesión)
